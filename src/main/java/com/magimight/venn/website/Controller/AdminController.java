146d2a22a35fd7f81bfb069bbb3da23556407040
Leo Devick <Leo Devick>
2025-06-02 16:58:37 -0500

    fixing update method of admin controller

--- src/main/java/com/magimight/venn/website/Controller/AdminController.java
+++ src/main/java/com/magimight/venn/website/Controller/AdminController.java
@@ -2,12 +2,14 @@ package com.magimight.venn.website.Controller;
 
 import com.magimight.venn.website.Model.AdminModel;
 import com.magimight.venn.website.Repository.AdminRepository;
+import com.mongodb.internal.bulk.UpdateRequest;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.repository.Update;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/admin")
 @Log
 public class AdminController {
 
@@ -29,7 +31,15 @@ public class AdminController {
     @PutMapping("/{id}")
     public AdminModel updateAdmin(@PathVariable String id, @RequestBody AdminModel adminModel) {
         AdminModel updatedAdmin = adminRepository.findById(id).orElseThrow();
-        updatedAdmin = adminModel;
+
+        if (adminModel.getEmail() != null) {
+            updatedAdmin.setEmail(adminModel.getEmail());
+        }
+
+        if (adminModel.getAdminLevel() != null) {
+            updatedAdmin.setAdminLevel(adminModel.getAdminLevel());
+        }
+
         return adminRepository.save(updatedAdmin);
     }
 
